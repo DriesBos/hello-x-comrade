250d6ac37728504f40771429b8ea20ac8ebb41ba
Dries Bos <info@driesbos.com>
2020-01-06 16:48:50 +0000

    page-synchronize data

--- pages/synchronize/index.vue
+++ pages/synchronize/index.vue
@@ -1,5 +1,40 @@
 <template>
   <div>
-    <p>TIME</p>
+    <p>{{ timeAMS }}</p>
+    <p>/CEST (Amsterdam)</p>
+    <p>{{ timeNY }}</p>
+    <p>/EST (New York)</p>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      timeAMS: "",
+      timeNY: ""
+    }
+  },
+  mounted() {
+    this.setTime(-5)
+  },
+  methods: {
+    setTime() {
+      let d = new Date()
+      let utc = d.getTime() + d.getTimezoneOffset() * 60000
+      var ny = new Date(utc + 3600000 * -5)
+      var ams = new Date(utc + 3600000 * 1)
+      this.timeAMS =
+        ams.getHours() +
+        ":" +
+        (ams.getMinutes() < 10 ? "0" : "") +
+        ams.getMinutes()
+      this.timeNY =
+        ny.getHours() +
+        ":" +
+        (ny.getMinutes() < 10 ? "0" : "") +
+        ny.getMinutes()
+    }
+  }
+}
+</script>
