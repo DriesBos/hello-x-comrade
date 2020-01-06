d06fad6a8494784f3275f75f55ebbbc388eee3e5
Dries Bos <info@driesbos.com>
2020-01-06 17:23:45 +0000

    header styling

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -2,13 +2,15 @@
   <header>
     <nav>
       <ul>
-        <nuxt-link to="/" tag="li">Home</nuxt-link>
         <nuxt-link to="/about" tag="li">About</nuxt-link>
         <nuxt-link to="/services" tag="li">Services</nuxt-link>
         <nuxt-link to="/synchronize" tag="li">Synchronize</nuxt-link>
         <nuxt-link to="/contact" tag="li">Contact</nuxt-link>
       </ul>
     </nav>
+    <nuxt-link to="/" tag="div" class="logo">
+      <div v-html="require('~/assets/images/logo.svg?include')" />
+    </nuxt-link>
   </header>
 </template>
 
