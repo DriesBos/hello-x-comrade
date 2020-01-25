3b2cad3ec855b899b0240363be481ea71b2052ef
Dries Bos <info@driesbos.com>
2020-01-25 15:02:05 +0000

    styling update

--- pages/synchronize/index.vue
+++ pages/synchronize/index.vue
@@ -24,32 +24,39 @@ export default {
     }
   },
   mounted() {
-    this.setTime()
+    this.setAmsterdamTime()
+    this.setLocalTime()
   },
   methods: {
-    setTime() {
-      let today = new Date()
-      let amsTimeZone = 2
-      var amsTime = new Date(today + 3600000 * amsTimeZone)
-      let userTimeZone = today.getTimezoneOffset() * 60000
-      var userTime = new Date(today + 3600000 * userTimeZone)
-      // console.log(today)
-      // console.log(amsTimeZone)
-      // console.log(amsTime)
-      // console.log(userTimeZone)
-      // console.log(userTime)
+    setAmsterdamTime() {
+      let date = new Date()
+      let offset = date.getTime() + date.getTimezoneOffset() * 60000
+      let amsTime = new Date(offset + 3600000 * 1)
       this.timeAMS =
         (amsTime.getHours() < 10 ? "0" : "") +
         amsTime.getHours() +
         ":" +
         (amsTime.getMinutes() < 10 ? "0" : "") +
         amsTime.getMinutes()
+      // console.log(date)
+      // console.log(offset)
+      // console.log(amsTime)
+      // console.log(this.timeAMS)
+    },
+    setLocalTime() {
+      let date = new Date()
+      let offset = date.getTimezoneOffset() * 60000
+      let userTime = new Date(date + 3600000 * offset)
       this.timeLocal =
         (userTime.getHours() < 10 ? "0" : "") +
         userTime.getHours() +
         ":" +
         (userTime.getMinutes() < 10 ? "0" : "") +
         userTime.getMinutes()
+      // console.log(date)
+      // console.log(offset)
+      // console.log(userTime)
+      // console.log(this.timeLocal)
     }
   }
 }
