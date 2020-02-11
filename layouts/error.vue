b3bf93c937b1c781794232b8ea3314b9c7e4e5e8
Dries Bos <info@driesbos.com>
2020-02-11 12:56:57 +0100

    init error page

---
+++ layouts/error.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="content">
+    <!-- prettier-ignore -->
+    <section class="content-Error">
+      <h1>404 ERROR 404 ERROR 404 ERROR 404 ERROR 404 ERROR 404 ERROR 404 ERROR 404 ERROR 404 ERROR 404 ERROR 404 ERROR 404 ERROR 404 ERROR 404 ERROR 404 ERROR 404 ERROR 404 ERROR 404 ERROR 404 ERROR 404 ERROR 404 ERROR 404 ERROR</h1>
+    </section>
+  </div>
+</template>
