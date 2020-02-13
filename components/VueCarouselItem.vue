037e7ca67793963668fae13e885d11cefd149a36
Dries Bos <info@driesbos.com>
2020-02-13 19:07:04 +0100

    carousel mobile nav

--- components/VueCarouselItem.vue
+++ components/VueCarouselItem.vue
@@ -5,10 +5,9 @@
         ref="vueCarouselItem"
         class="carousel-Container"
         :per-page="1"
-        :minSwipeDistance="8"
         :mouse-drag="false"
         :paginationEnabled="false"
-        :speed="160"
+        :speed="190"
       >
         <slide
           class="carousel-Slide"
@@ -29,6 +28,20 @@
         @click="SlideCarousel('next')"
       />
     </div>
+    <div class="carousel-Mobile_Navigation">
+      <a
+        class="carousel-Mobile_Nav carousel-Mobile_Nav_Prev"
+        @click="SlideCarousel('prev')"
+      >
+        <img src="~/assets/images/arrow-right.svg"
+      /></a>
+      <a
+        class="carousel-Mobile_Nav carousel-Mobile_Nav_Next"
+        @click="SlideCarousel('next')"
+      >
+        <img src="~/assets/images/arrow-right.svg"
+      /></a>
+    </div>
   </div>
 </template>
 
@@ -42,8 +55,8 @@ export default {
     images: Array
   },
   mounted() {
-    $(".carousel-Nav_Prev").on("mouseover", this.changeCursorToPrev)
-    $(".carousel-Nav_Next").on("mouseover", this.changeCursorToNext)
+    $(".carousel-Nav_Prev").on("mousemove", this.changeCursorToPrev)
+    $(".carousel-Nav_Next").on("mousemove", this.changeCursorToNext)
     $(".carousel-Nav_Prev").on("mouseleave", this.removeCursor)
     $(".carousel-Nav_Next").on("mouseleave", this.removeCursor)
   },
@@ -52,25 +65,47 @@ export default {
       const carousel = this.$refs["vueCarouselItem"]
       const currentPage = carousel.currentPage
       const pageCount = carousel.pageCount
+      let $cursor = $(".cursor")
       if (value == "prev") {
-        currentPage != 0
-          ? carousel.goToPage(currentPage - 1)
-          : carousel.goToPage(pageCount - 1)
+        if (currentPage != 0) {
+          carousel.goToPage(currentPage - 1)
+        } else {
+          // carousel.goToPage(currentPage + 1)
+          $cursor.removeClass("cursor-Prev")
+        }
       } else {
-        currentPage < pageCount - 1
-          ? carousel.goToPage(currentPage + 1)
-          : carousel.goToPage(0)
+        if (currentPage < pageCount - 1) {
+          carousel.goToPage(currentPage + 1)
+        } else {
+          // carousel.goToPage(currentPage - 1)
+          $cursor.removeClass("cursor-Next")
+        }
       }
     },
     changeCursorToPrev() {
+      const carousel = this.$refs["vueCarouselItem"]
+      const currentPage = carousel.currentPage
       let $cursor = $(".cursor")
-      $cursor.addClass("cursor-Prev")
-      $cursor.removeClass("cursor-Next")
+      if (currentPage != 0) {
+        $cursor.addClass("cursor-Prev")
+        $cursor.removeClass("cursor-Next")
+      } else {
+        $cursor.removeClass("cursor-Next")
+        $cursor.removeClass("cursor-Prev")
+      }
     },
     changeCursorToNext() {
+      const carousel = this.$refs["vueCarouselItem"]
+      const currentPage = carousel.currentPage
+      const pageCount = carousel.pageCount
       let $cursor = $(".cursor")
-      $cursor.addClass("cursor-Next")
-      $cursor.removeClass("cursor-Prev")
+      if (currentPage < pageCount - 1) {
+        $cursor.addClass("cursor-Next")
+        $cursor.removeClass("cursor-Prev")
+      } else {
+        $cursor.removeClass("cursor-Prev")
+        $cursor.removeClass("cursor-Next")
+      }
     },
     removeCursor() {
       let $cursor = $(".cursor")
@@ -87,6 +122,7 @@ export default {
   &-Container
     position: relative
     height: 100%
+    background-color: black
     .VueCarousel-wrapper, .VueCarousel-inner, .VueCarousel-slide
       position: relative
       height: 100% !important // Override dep default
@@ -107,4 +143,26 @@ export default {
       left: 0
     &_Next
       right: 0
+  &-Mobile
+    &_Navigation
+      position: absolute
+      left: var(--side-padding)
+      bottom: var(--side-padding)
+      right: var(--side-padding)
+      flex-wrap: nowrap
+      z-index: 999
+      // transform: translateY(-50%)
+      mix-blend-mode: difference
+      display: none
+      @media ( hover: none )
+        display: flex
+      a
+        width: 50vw
+        img
+          height: 1.5em
+      a:first-child
+        img
+          transform: rotate(180deg)
+      a:last-child
+        text-align: right
 </style>
