6dfa2690f26ba8767ea825a1ec0a9b97960ae208
Dries Bos <info@driesbos.com>
2020-06-08 21:27:27 +0200

    build

--- static/sw.js
+++ static/sw.js
@@ -2,32 +2,32 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/0c668e2cc06acd346edc.js",
-    "revision": "0438b50d363af18706baba04f3c6a2a8"
-  },
-  {
-    "url": "/_nuxt/0f3dc5ea18084b9b623d.js",
-    "revision": "59530b72b7b610116415b872477befcc"
-  },
-  {
-    "url": "/_nuxt/1a40278e2148551fa301.js",
-    "revision": "e70152c48e48718912a6a4ac22245b32"
-  },
-  {
-    "url": "/_nuxt/5a3b3c0aa7d764056d2b.js",
-    "revision": "35c3795abb387d445e52059cb55580e3"
-  },
-  {
-    "url": "/_nuxt/99ac7c8b1c1963c34ae1.js",
+    "url": "/_nuxt/0aa655c9a524f0b8ed73.js",
     "revision": "0730a53ef8397b3b97984ad00d2af444"
   },
   {
-    "url": "/_nuxt/a1537674a5d935242116.js",
-    "revision": "7de4b09760b5325247a5d7d6ab78e4bc"
+    "url": "/_nuxt/1275f904aaf3f357bb56.js",
+    "revision": "9ca824507a9c68b6a1befe68e4acc861"
   },
   {
-    "url": "/_nuxt/d83afeadbba6461aa262.js",
+    "url": "/_nuxt/69401e45e534d546dd9b.js",
     "revision": "f1e4194bdaf9e1cd2206fc9ab219c241"
+  },
+  {
+    "url": "/_nuxt/b539e639146be549645b.js",
+    "revision": "c329ac023e9981ec8e8856fd53cfde95"
+  },
+  {
+    "url": "/_nuxt/dcfda56e050928708c28.js",
+    "revision": "34a88a24cb47c9140c79895812b5c53a"
+  },
+  {
+    "url": "/_nuxt/ed657854f226741cf716.js",
+    "revision": "bc4cb5beac093b579b06cfcc64e929ba"
+  },
+  {
+    "url": "/_nuxt/ff9f4cb5f98f8c5bfb69.js",
+    "revision": "e70152c48e48718912a6a4ac22245b32"
   }
 ], {
   "cacheId": "nuxt-storyblok-boilerplate",
